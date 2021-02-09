9c7f121c7e8f3bf72b1a19c90f54d4c640e46f85
Anna Vekhteva <gnedenko.anna@gmail.com>
2021-02-09 22:36:48 +0300

    Now gitlab go terraform

---
+++ gitlab-ci/terraform/variables.tf
@@ -0,0 +1,25 @@
+variable project {
+  description = "Project ID"
+}
+variable region {
+  description = "Region"
+  # Значение по умолчанию
+  default = "europe-west1"
+}
+variable instance_zone {
+  description = "Region"
+  # Значение по умолчанию
+  default = "europe-west1-b"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable private_key_path {
+  # Описание переменной
+  description = "Path to the private key used for ssh access"
+}
+variable disk_image {
+  description = "Disk image for gitlab-ci (ubuntu)"
+  default = "ubuntu-1604-xenial-v20210203"
+}
